925a71a14e80523d783e82dba96512a7e7a8e589
Elias Frigård <elias.frigard@tvtools.fi>
2024-10-10 17:11:53 +0300

    more data

---
+++ prisma/migrations/20241010135322_drop_primary_bodypart_boolean/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isPrimary` on the `BodyPartExercise` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BodyPartExercise" DROP COLUMN "isPrimary";
--- src/app/components/ExerciseSelector.tsx
+++ src/app/components/ExerciseSelector.tsx
@@ -9,13 +9,14 @@ interface ExerciseSelectorProps {
 
 const ExerciseSelector: React.FC<ExerciseSelectorProps> = (props) => {
   return (
-    <div className='container w-full'>
+    <div className='container w-full flex flex-col gap-8'>
       <h1>Exercise Selector</h1>
 
       {
         props.bodyParts.map((bodyPart) => (
-          <div key={bodyPart.id}>
-            <h2 className='text-2xl font-bold'>{bodyPart.name}</h2>
+          <div key={bodyPart.id} className='flex flex-col gap-6'>
+            <h2 className='text-3xl font-bold'>{bodyPart.name}</h2>
+            <div className='h-[1px] w-full bg-slate-950 rounded-full opacity-20' />
             <div className='grid grid-cols-3 gap-4'>
               {
                 bodyPart.exercises.map((exercise) => (
